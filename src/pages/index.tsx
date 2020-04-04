3ae6d221393cb703b68bdadf42146b90cf334dde
Hossam Magdy <hossam.magdy@outlook.com>
2020-04-05 01:13:54 +0200

    Add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Head from "next/head";
 
 const TEXT = `(This will be built with some decent frontend tech ;) … later;`;
 
 const Home: React.FC = () => {
-  return <pre>{TEXT}</pre>;
+  return (
+    <>
+      <Head>
+        <title>Hossam Magdy</title>
+      </Head>
+      <pre>{TEXT}</pre>
+    </>
+  );
 };
 
 export default Home;
