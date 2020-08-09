ad5fe79c2c58e72331a55181fddb4f98b2b8a590
Hossam Magdy <hossam.magdy@outlook.com>
2020-08-09 09:31:05 +0200

    fix(build): update imports & types of React, ReactDOM[Server]
    fix(build): drop types of module (react-dom)
    as removed from deno.land/x
    
    fix: upgrade std lib to 0.64.0
    
    fix(build,bundle): use React from "unpkg.com/es-react" for client bundle
    
    fix(build): use ReactDOM from PikaCDN
    
    as it provides typings (but typings not working with react-dom/server,
    only react-dom)
    see: https://www.reddit.com/r/Deno/comments/gqft4o/wrapping_existing_npm_packages/frswus7/
    
    fix(build): patch the custom ReactDOMServer typings
    
    fix(build): use ReactDOMServer from JSPM.IO

--- deps_client.ts
+++ deps_client.ts
@@ -1,8 +1,21 @@
-// #region React     // see: https://deno.land/x/types/react-dom
-// @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
-import * as React from "https://cdn.pika.dev/@pika/react@v16.13.1";
-// @deno-types="https://deno.land/x/types/react-dom/v16.13.1/react-dom.d.ts"
-import * as ReactDOM from "https://cdn.pika.dev/@pika/react-dom@v16.13.1"; // can be "* as" (VSCode recognized) or "default as" (unknow in VSCode)
+// #region React
+
+// X(404) @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
+// X @deno-types="https://cdn.jsdelivr.net/npm/@types/react@16.9.45/index.d.ts"
+import * as React from "https://unpkg.com/es-react@16.13.1/index.js"; // BUNDLING WORKS
+// import * as React from "https://cdn.pika.dev/react"; // TYPINGS WORKS (BUNDLE NOT)
+// import * as React from "https://cdn.skypack.dev/react";
+// import * as React from "https://cdn.pika.dev/@pika/react@v16.13.1";
+
+// X(404) @deno-types="https://deno.land/x/types/react-dom/v16.13.1/react-dom.d.ts"
+// X @deno-types="https://cdn.jsdelivr.net/npm/@types/react-dom@16.9.8/index.d.ts"
+import * as ReactDOM from "https://cdn.pika.dev/react-dom";
+// import * as ReactDOM from "https://cdn.skypack.dev/react-dom";
+// import * as ReactDOM from "https://unpkg.com/react-dom@16.13.1/umd/react-dom.production.min.js"; // can be "* as" (VSCode recognized) or "default as" (unknow in VSCode)
+// import * as ReactDOM from "https://cdn.pika.dev/@pika/react-dom@v16.13.1"; // can be "* as" (VSCode recognized) or "default as" (unknow in VSCode)
+
+// X(backup) import { default as ReactDOMServer } from "https://jspm.dev/react-dom/server.js";
+
 // export
 export { React, ReactDOM };
 // #endregion
--- deps_server.ts
+++ deps_server.ts
@@ -8,13 +8,29 @@
 export {
   serve as http_serve,
   ServerRequest,
-} from "https://deno.land/std@0.55.0/http/server.ts";
+} from "https://deno.land/std@0.64.0/http/server.ts";
 export const PORT = parseInt(Deno.env.get("PORT") || "8080");
 // #endregion
 
-export * from './deps_client.ts';
-// @deno-types="https://deno.land/x/types/react-dom/v16.13.1/server.d.ts"
-import ReactDOMServer from "https://dev.jspm.io/react-dom@16.13.1/server.js";
+export * from "./deps_client.ts";
+
+// The following way to use ReactDOMServer is temporarily untill PikaCDN supports react-dom/server with types
+// ref: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/react-dom/server/index.d.ts
+import _ReactDOMServer from "https://dev.jspm.io/react-dom/server";
+interface IReactDOMServer {
+  renderToString(element: JSX.Element): string;
+  renderToStaticMarkup(element: JSX.Element): string;
+}
+const ReactDOMServer: IReactDOMServer = _ReactDOMServer as any;
+
+// see https://www.reddit.com/r/Deno/comments/gqft4o/wrapping_existing_npm_packages/frswus7/
+// X @den o-types="https://cdn.jsdelivr.net/npm/@types/react-dom@16.9.8/server/index.d.ts"
+// X @den o-types="https://cdn.pika.dev/-/react-dom@v16.13.1-qv1YB4ZAVXJG84jEgino/dist=es2019/server,mode=types/server.d.ts"
+// import _ReactDOMServer from "https://cdn.pika.dev/react-dom/server"; // imports ReactDOM not ReactDOMServer!
+// import * as _ReactDOMServer from "https://jspm.dev/react-dom@16.13.1/server";
+// import ReactDOMServer from "https://dev.jspm.io/react-dom/server";
+// import ReactDOMServer from "https://dev.jspm.io/react-dom@16.13.1/server.js";
+
 // export
 export { ReactDOMServer };
 // #endregion
