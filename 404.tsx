43014360ad36bd3b9bef1526922767588ba60d9c
Hossam Magdy <hossam.magdy@outlook.com>
2020-12-21 21:15:42 +0100

    fix(google-analytics): was not loading when extracted in separate component

--- 404.tsx
+++ 404.tsx
@@ -1,15 +1,19 @@
 import React from "https://esm.sh/react";
 
-import { Head } from "https://deno.land/x/aleph/mod.ts";
-
-import GoogleAnalyticsScript from "./components/google-analytics-script.tsx";
+import { Head, Scripts } from "https://deno.land/x/aleph/mod.ts";
 
 export default function App404() {
   return (
     <>
       <Head>
         <meta http-equiv="refresh" content="0;URL='/'" />
-        <GoogleAnalyticsScript />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6009068-7" />
+        <script>
+          {`window.dataLayer = window.dataLayer || [];`}
+          {`function gtag(){dataLayer.push(arguments);}`}
+          {`gtag('js', new Date());`}
+          {`gtag('config', 'UA-6009068-7');`}
+        </script>
       </Head>
     </>
   );
--- app.tsx
+++ app.tsx
@@ -1,8 +1,6 @@
 import React, { ComponentType } from "https://esm.sh/react";
 
-import { Head } from "https://deno.land/x/aleph/mod.ts";
-
-import GoogleAnalyticsScript from "./components/google-analytics-script.tsx";
+import { Head, Scripts } from "https://deno.land/x/aleph/mod.ts";
 
 export default function App(
   { Page, pageProps }: { Page: ComponentType<any>; pageProps: any },
@@ -12,7 +10,13 @@ export default function App(
       <Head>
         <title>Hossam Magdy</title>
         <link rel="icon" href="/favicon.svg" />
-        <GoogleAnalyticsScript />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-6009068-7" />
+        <script>
+          {`window.dataLayer = window.dataLayer || [];`}
+          {`function gtag(){dataLayer.push(arguments);}`}
+          {`gtag('js', new Date());`}
+          {`gtag('config', 'UA-6009068-7');`}
+        </script>
       </Head>
       <Page {...pageProps} />
     </>
