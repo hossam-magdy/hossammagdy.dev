86a79d862bd4adcafb4b374dc3c7061770d808be
Hossam Magdy <hossam.magdy@outlook.com>
2021-12-19 14:15:49 +0100

    chore: update getServerData

--- src/utils/getServerData.ts
+++ src/utils/getServerData.ts
@@ -1,15 +1,8 @@
 import { ServerRequest } from "http";
-import { reformObject } from "utils/reformObject.ts";
 
 const getDenoData = () => ({
   "version": Deno.version,
-  // "args": Deno.args, // always [] in GoogleCloudRun
-  // "build": Deno.build, // always on "linux x86_64 gnu" in GoogleCloudRun
-  "env.toObject()": reformObject(Deno.env.toObject()),
-  // "execPath": Deno.execPath(), // --allow-read
-  // "hostname()": Deno.hostname(), // --unstable
-  // "permissions": Deno.permissions.query({ name: "env" }), // many permissions
-  // "pid": Deno.pid, // always 1 in GoogleCloudRun
+  "K_REVISION": Deno.env.get("K_REVISION"),
 });
 
 export const getServerData = (req?: ServerRequest) => (JSON.stringify(
@@ -20,7 +13,7 @@ export const getServerData = (req?: ServerRequest) => (JSON.stringify(
       url: req?.url,
       headers: req?.headers,
     },
-    Deno: getDenoData(),
+    server: getDenoData(),
   },
   null,
   2,
